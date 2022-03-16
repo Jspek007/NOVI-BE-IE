bef7985f2628a5d6ce9324f2b2ddbf51bc2451ce
Johan Spekman <johanspekman@gmail.com>
2022-03-16 21:12:37 +0100

    Make repo final

--- src/main/java/johan/spekman/novibeie/module_product/product/service/ProductServiceImpl.java
+++ src/main/java/johan/spekman/novibeie/module_product/product/service/ProductServiceImpl.java
@@ -16,7 +16,7 @@ import java.util.List;
 @Service
 @Transactional
 public class ProductServiceImpl implements ProductService {
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
     public ProductServiceImpl(ProductRepository productRepository) {
         this.productRepository = productRepository;
