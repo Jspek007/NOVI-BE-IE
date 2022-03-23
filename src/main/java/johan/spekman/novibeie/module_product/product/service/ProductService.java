42b9b6c9cdc8d32c59cec8e17c7c3437a0d2f9cb
Johan Spekman <johanspekman@gmail.com>
2022-03-23 15:14:16 +0100

    Add delete and update methods

--- src/main/java/johan/spekman/novibeie/module_product/product/service/ProductService.java
+++ src/main/java/johan/spekman/novibeie/module_product/product/service/ProductService.java
@@ -1,5 +1,6 @@
 package johan.spekman.novibeie.module_product.product.service;
 
+import johan.spekman.novibeie.module_customer.dto.CustomerDto;
 import johan.spekman.novibeie.module_product.product.dto.ProductDto;
 import johan.spekman.novibeie.module_product.product.model.Product;
 import org.springframework.http.ResponseEntity;
@@ -11,4 +12,7 @@ import java.util.List;
 public interface ProductService {
     ResponseEntity<Object> createProduct(@Valid ProductDto productDto, BindingResult bindingResult);
     List<Product> getAllProducts();
+    Product getProductBySku(String sku);
+    void deleteProductBySku(String sku);
+    ResponseEntity<Object> updateProduct(String sku, ProductDto productDto, BindingResult bindingResult);
 }
--- src/main/java/johan/spekman/novibeie/module_product/product/service/ProductServiceImpl.java
+++ src/main/java/johan/spekman/novibeie/module_product/product/service/ProductServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
 import javax.transaction.Transactional;
 import java.util.Date;
@@ -25,6 +26,41 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public List<Product> getAllProducts() {
         return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProductBySku(String sku) {
+        return productRepository.findBySku(sku);
+    }
+
+    @Override
+    public void deleteProductBySku(String sku) {
+        Long productId = productRepository.findBySku(sku).getId();
+        productRepository.deleteById(productId);
+        ResponseEntity.ok("Product with sku: " + sku + " has been deleted.");
+    }
+
+    @Override
+    public ResponseEntity<Object> updateProduct(String sku, ProductDto productDto, BindingResult bindingResult) {
+        InputValidation inputValidation = new InputValidation();
+        if (inputValidation.validate(bindingResult) != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(inputValidation.validate(bindingResult));
+        } else {
+            Product existingProduct = productRepository.findBySku(sku);
+
+            if (existingProduct == null) {
+                return null;
+            } else {
+                existingProduct.setSku(productDto.getSku());
+                existingProduct.setProductTitle(productDto.getProductTitle());
+                existingProduct.setProductDescription(productDto.getProductDescription());
+                existingProduct.setProductPrice(productDto.getProductPrice());
+                existingProduct.setEnabled(productDto.isEnabled());
+                productRepository.save(existingProduct);
+
+                return new ResponseEntity<>(existingProduct, HttpStatus.OK);
+            }
+        }
     }
 
     @Override
