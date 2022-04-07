5b5feac7f191b8e3bd062ac7f9b241b5d7e1e763
Johan Spekman <johanspekman@gmail.com>
2022-04-07 15:27:28 +0200

    Add sales_order controller tests

--- src/test/java/johan/spekman/novibeie/module_order/controller/SalesOrderControllerTest.java
+++ src/test/java/johan/spekman/novibeie/module_order/controller/SalesOrderControllerTest.java
@@ -2,15 +2,23 @@ package johan.spekman.novibeie.module_order.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import johan.spekman.novibeie.module_orders.model.SalesOrder;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import johan.spekman.novibeie.module_orders.controller.SalesOrderController;
 import johan.spekman.novibeie.module_orders.service.SalesOrderService;
+import org.springframework.validation.BindingResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 @ContextConfiguration(classes = SalesOrderController.class)
@@ -25,4 +33,23 @@ public class SalesOrderControllerTest {
     @MockBean
     private SalesOrderService salesOrderService;
 
+
+    @Mock
+    SalesOrder salesOrder;
+
+
+    @Test
+    public void whenValidInput_thenReturn200() throws Exception {
+        mockMvc.perform(post("/api/v1/sales_orders/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(salesOrder)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void whenInvalidInput_thenReturn400() throws Exception {
+        mockMvc.perform(post("/api/v1/sales_orders/create")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
 }
